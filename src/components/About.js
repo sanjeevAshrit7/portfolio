00632f5906c56ad156b6a81b613864730968d3f7
sanjeevAshrit7 <sanjeev@zerozilla.com>
2022-10-19 02:35:46 +0530

    deploy warnings commit7

--- src/components/About.js
+++ src/components/About.js
@@ -24,10 +24,10 @@ function About() {
     // console.log('author', author)
     return (
         <main className='relative'>
-            <img src={bg} alt={'NA'} className={'absolute w-full '} />
+            <img src={bg} alt={'scene'} className={'absolute w-full '} />
             <div className='p-10 lg:pt-48 containe mx-auto relative'>
                 <section className='bg-green-800 rounded-lg shadow-2xl lg:flex p-20'>
-                    <img src={author?.authorImage} className={'rounded w-32 h-32 lg:w-64 lg:h-64 mr-8'} alt={'no author'} />
+                    <img src={author?.authorImage} className={'rounded w-32 h-32 lg:w-64 lg:h-64 mr-8'} alt={'author'} />
                     <div className='text-lg flex flex-col justify-center'>
                         <h1
                             className='cursive text-6xl text-green-300 mb-4'
