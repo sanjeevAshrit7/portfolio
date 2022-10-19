6b40a60fd00af7c508aa5a64f7cbe7c25e66367b
sanjeevAshrit7 <sanjeev@zerozilla.com>
2022-10-19 10:11:23 +0530

    deploy warnings commit8

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
     <main>
         <img src={image} alt={"Home NA"} className={"absolute object-cover w-full h-full"}/>
         <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8 justify-center'>
-            <h1 className='text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name'>Aloha... i am Sanjeev</h1>
+            <h1 className='text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name text-center'>Aloha... I am Sanjeev</h1>
         </section>
     </main>
   )
